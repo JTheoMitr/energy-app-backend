4611afe4a380b8b12474c08cc46be0c9d3b76801
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-05-09 14:00:51 -0500

    added create and delete functions to energies controller

--- app/controllers/energies_controller.rb
+++ app/controllers/energies_controller.rb
@@ -5,4 +5,25 @@ class EnergiesController < ApplicationController
         render json: EnergySerializer.new(energies, include: [:companies])
     end
 
+    def create
+        energy = Energy.new(energy_params)
+        if energy.save
+            render json: EnergySerializer.new(energy)
+        else
+            render json: {error: "Could not save Energy"}
+        end
+    end
+
+    def destroy
+        energy = Energy.find(params[:id])
+        energy.destroy
+        render json: {message: "ENERGY DELETED: #{energy.name}"}
+    end
+
+    private
+
+    def energy_params
+        params.require(:energy).permit(:name)
+    end
+
 end
